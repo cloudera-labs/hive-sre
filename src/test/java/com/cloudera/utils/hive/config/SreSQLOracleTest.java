afdf2b6022746cebe92367b0b71a58f4fc17559d
David W. Streever <david@streever.com>
2022-11-09 10:30:35 -0500

    Optimized Nested Table Locations check in `sre`. https://github.com/cloudera-labs/hive-sre/issues/27

---
+++ src/test/java/com/cloudera/utils/hive/config/SreSQLOracleTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022. Cloudera, Inc. All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.cloudera.utils.hive.config;
+
+import com.cloudera.utils.hive.Sre;
+import org.junit.Test;
+
+public class SreSQLOracleTest {
+
+    @Test
+    public void u3ORACLE_SQLTest() {
+        String cfg = System.getProperty("user.home") + System.getProperty("file.separator") +
+                ".hive-sre/cfg" + System.getProperty("file.separator") +
+                "default-" + Metastore.DB_TYPE.ORACLE + ".yaml";
+        Sre.main(new String[]{"u3", "-tsql", "-all", "-cfg", cfg,"-o", "/tmp/sre-sql-test"});
+    }
+
+    @Test
+    public void sreORACLE_SQLTest() {
+        String cfg = System.getProperty("user.home") + System.getProperty("file.separator") +
+                ".hive-sre/cfg" + System.getProperty("file.separator") +
+                "default-" + Metastore.DB_TYPE.ORACLE + ".yaml";
+        Sre.main(new String[]{"sre", "-tsql", "-all", "-cfg", cfg,"-o", "/tmp/sre-sql-test"});
+    }
+
+}
